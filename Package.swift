1aac7ee1b5dda3d370430f78be0bd8d5253202a7
w9i <50361448+w9i@users.noreply.github.com>
2022-05-18 17:08:27 +0800

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["Networking"]),
     ],
     dependencies: [
-        .package(path: "../Common")
+        .package(name: "Common", url: "https://github.com/w9i/Common", from: "1.0.0"),
     ],
     targets: [
         .target(
